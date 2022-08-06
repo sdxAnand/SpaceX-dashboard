f4a552f0e56c890ac261cfdecb4abb2a8cef2681
sudhansh <anandsudhansh@gmail.com>
2022-08-06 13:13:56 +0530

    fisrt commit

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+           	&#169;2021-22
+            Developed By: Sudhansh Anand
+        </div>
+    );
+}
+
+export default Footer;
